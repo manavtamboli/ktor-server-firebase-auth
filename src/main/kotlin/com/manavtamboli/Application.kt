09d0a487dfa8203b88d1e5fcb2a5eb9b19ab287b
manavtamboli <manavtamboli8@gmail.com>
2021-11-18 23:57:39 +0530

    Test for unauthorized request added. Application configuration improvements.

--- src/main/kotlin/com/manavtamboli/Application.kt
+++ src/main/kotlin/com/manavtamboli/Application.kt
@@ -1,15 +1,49 @@
 package com.manavtamboli
 
+import com.google.firebase.FirebaseApp
+import com.manavtamboli.FirebaseAuthentication.AuthorizationType
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.cio.*
 import io.ktor.server.engine.*
 
 fun main() {
     embeddedServer(CIO, port = 8080, host = "127.0.0.1") {
-        routing {
-            get("/"){
+        configureApplication()
+    }.start(wait = true)
+}
 
+fun Application.configureApplication(){
+
+    // Initialize FirebaseApp.
+    initializeFirebase()
+
+    // Configure FirebaseAuthentication
+    install(FirebaseAuthentication) {
+        // Sets the FirebaseApp instance to verify tokens.
+        // If not specified, default FirebaseApp instance will be used.
+        firebaseApp = FirebaseApp.getInstance()
+
+        // Sets the authorization type used in the requests.
+        // Default is AuthorizationType.Bearer
+        authorizationType = AuthorizationType.Bearer
+
+        // Sets whether to check if the tokens were revoked or if the user is disabled.
+        // Defaults to false.
+        checkRevoked = true
+    }
+
+    // Configure Routing
+    routing {
+        get("/test"){
+            val decodedToken = call.getDecodedToken()
+            if (decodedToken == null){
+                call.respond(HttpStatusCode.Unauthorized)
+            } else {
+                call.respond(HttpStatusCode.OK)
             }
         }
-    }.start(wait = true)
+    }
 }
--- src/test/kotlin/com/manavtamboli/ApplicationTest.kt
+++ src/test/kotlin/com/manavtamboli/ApplicationTest.kt
@@ -1,20 +1,18 @@
 package com.manavtamboli
 
-import io.ktor.routing.*
 import io.ktor.http.*
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import kotlin.test.*
 import io.ktor.server.testing.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
     fun testRoot() {
-        withTestApplication({  }) {
-            handleRequest(HttpMethod.Get, "/").apply {
-                assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals("Hello World!", response.content)
+        withTestApplication({ configureApplication() }) {
+
+            // Test Unauthorized
+            handleRequest(HttpMethod.Get, "/test").apply {
+                assertEquals(HttpStatusCode.Unauthorized, response.status())
             }
         }
     }
