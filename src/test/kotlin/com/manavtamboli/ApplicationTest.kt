5cdf0d669503cdaf825c6a0190786fb02fa2067b
manavtamboli <manavtamboli8@gmail.com>
2021-11-19 00:21:27 +0530

    Tests added.

--- src/test/kotlin/com/manavtamboli/ApplicationTest.kt
+++ src/test/kotlin/com/manavtamboli/ApplicationTest.kt
@@ -10,10 +10,32 @@ class ApplicationTest {
     fun testRoot() {
         withTestApplication({ configureApplication() }) {
 
-            // Test Unauthorized
+            // Test Null token
             handleRequest(HttpMethod.Get, "/test").apply {
+                assertEquals(HttpStatusCode.BadRequest, response.status())
+            }
+
+            // Test Blank token
+            handleRequest(HttpMethod.Get, "/test"){
+                addHeader(HttpHeaders.Authorization, "Bearer ")
+            }.apply {
+                assertEquals(HttpStatusCode.BadRequest, response.status())
+            }
+
+            // Test Invalid token
+            handleRequest(HttpMethod.Get, "/test"){
+                addHeader(HttpHeaders.Authorization, "Bearer ${generateRandomString()}")
+            }.apply {
                 assertEquals(HttpStatusCode.Unauthorized, response.status())
             }
         }
+    }
+
+    private fun generateRandomString(length : Int = 64) : String {
+        val charPool : List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+        return (1..length)
+            .map { kotlin.random.Random.nextInt(0, charPool.size) }
+            .map(charPool::get)
+            .joinToString("")
     }
 }
